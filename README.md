a0b91f7ca94427f8c56ad1628bf484fbc19f3e27
Pablo <pablosjv@hotmail.com>
2016-12-19 11:46:11 +0100

    Extepcion añadida cuando haya parametros incorrectos

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # ml-modeling
 
-Este proyecto servirá para las tareas de ml-modeling en el Lab del BBVA. El objetivo actual es lanzar varias instancias de un stack desde el catalogo de rancher con diferentes configuraciones que se darán mediante una lista. Este proyecto tiene dos partes diferenciadas. Una es la parte donde se aloja el script de python y su correspondiente dockerizacion. La segunda es la carpeta que añadirá este servicio como stack del catalogo de rancher para que se pueda lanzar desde ahi. Estas dos partes se describirán más en detalle a continuación.
+Este proyecto servirá para las tareas de ml-modeling. El objetivo actual es lanzar varias instancias de un stack desde el catalogo de rancher con diferentes configuraciones que se darán mediante una lista. Este proyecto tiene dos partes diferenciadas. Una es la parte donde se aloja el script de python y su correspondiente dockerizacion. La segunda es la carpeta que añadirá este servicio como stack del catalogo de rancher para que se pueda lanzar desde ahi. Estas dos partes se describirán más en detalle a continuación.
 
-##Nota sobre la version actual
+## IMPORTANTE: Notas sobre la version actual
 
 Para pruebas, ahora mismo esta configurado el script de python de tal forma que se borran los stacks lanzados pasado un tiempo determinado. De momento solo queremos comprobar que lanza los stacks correctamente desde rancher y docker.
+Este parametr
 
 ## dockerizacion python script
 
@@ -47,6 +48,8 @@ docker run -it --rm --name my-running-app-name lanzador-python
 ```
 ##Comando para ejecutar lanzadorServicios
 
+<!-- NOTA IMPORTANTE: Recuerda comprobar la ip donde se hubican los host del rancher, debe ser la privada -->
+
 ```
-python lanzadorServicios.py https://dl.dropboxusercontent.com/u/92981874/entradas.yml 377EC393AE145A755881 chk1Le5mmAJAMfB1ddNLbyL5yEC4sDPKmCV28bEL http://185.24.5.232:8080/ http://185.24.5.232:8080/v1-catalog/templates/myRancher-Catalog:TestCatalog:0
+python lanzadorServicios.py http://ml-modeling.neocities.org/entradas.txt 377EC393AE145A755881 chk1Le5mmAJAMfB1ddNLbyL5yEC4sDPKmCV28bEL http://185.24.5.232:8080/ http://185.24.5.232:8080/v1-catalog/templates/myRancher-Catalog:TestCatalog:0
 ``
--- service/lanzadorServicios.py
+++ service/lanzadorServicios.py
@@ -17,11 +17,9 @@ def stopService(name_stack):
         '--secret-key', secret_key,
         'rm', '--stop', name_stack])
 
-#Set up del logger
 # logger = logging.getLogger('services_launcher')
 
 logging.critical('ENTRÓ EN EL LANZADOR DE STACKS')
-#sys.stdout.write("ENTRÓ EN EL LANZADOR DE STACKS\n")
 #project_name = 'Model'
 cont = 0
 parametros=[]
@@ -32,19 +30,14 @@ time_out = 30.0
 #Lectura de parametros para las url y las keys
 url_entradas = str(sys.argv[1])
 logging.critical('url de las entradas:'+url_entradas)
-# sys.stdout.write("url de las entradas:"+url_entradas+"\n")
 access_key = str(sys.argv[2])
 logging.critical('access key:'+access_key)
-# sys.stdout.write("access key:"+access_key+"\n")
 secret_key = str(sys.argv[3])
 logging.critical('secret key:'+secret_key)
-# sys.stdout.write("secret key:"+secret_key+"\n")
 url = str(sys.argv[4])
 logging.critical('url del rancher:'+url)
-# sys.stdout.write("url del rancher:"+url+"\n")
 url_catalog = str(sys.argv[5])
 logging.critical('url del stack a lanzar:'+url_catalog)
-# sys.stdout.write("url del stack a lanzar:"+url_catalog+"\n")
 
 
 #Peticion a la API para obtener el dockercompose
@@ -52,35 +45,34 @@ auth = requests.auth.HTTPBasicAuth(access_key, secret_key)
 r = requests.get(url=url_catalog, auth=auth)
 content_all = r.json()
 logging.critical('Obtenido el objeto JSON de la API')
-# sys.stdout.write("Obtenido el objeto JSON de la API\n")
-content_dockercompose = str(content_all["files"]["docker-compose.yml"])
 docker_compose = open('docker-compose.yml', 'w')
 docker_compose.write(content_dockercompose)
 docker_compose.close()
 
 entradas = requests.get(url=url_entradas, verify=False)
 entradas = yaml.load(entradas.text)
-logging.critical('Obtenido el fichero de configuracion')
-# sys.stdout.write("Obtenido el fichero de configuracion\n")
+logging.critical('Obtenido el fichero de configuracion para los parametros')
 
 #Las distintas formas que se consideran son: parametroNombre->n
 #1. [valorInicial:valorFinal:Salto] -> Lineal
-#2. [valorInicial:valorFinal:Función] -> Otro tipo de funcion
 #3. [un String]
 for parametro in entradas:
     parametrosNombre.append(parametro)
-    opcion = entradas[parametro]["type"] #parametro[parametro.index("{"):parametro.index("}")]
-    if(opcion=="lineal"):
-        valorInicial = entradas[parametro]["initial-value"]
+    opcion = entradas[parametro]['type'] #parametro[parametro.index("{"):parametro.index("}")]
+    if(opcion=='lineal'):
+        valorInicial = entradas[parametro]['initial-value']
         valorFinal = entradas[parametro]["final-value"]
         valorSalto = entradas[parametro]["interval"]
         opcionesParametro = numpy.arange(valorInicial, valorFinal, valorSalto)
         parametros.append(opcionesParametro.tolist())
-    # elif(opcion==2):
-    #     #opcionesParametro
-    #     pass
+    elif(opcion==2):
+        #opcionesParametro
+        pass
     elif(opcion=="absolute"):
         parametros.append(entradas[parametro]["param"])
+    else:
+        logging.critical('ERROR: FORMATO DE PARAMETROS INCORRECTO')
+        raise SyntaxError('Parametros en el yml de entradas incorectos')
 parametrosNombre = parametrosNombre[::-1]
 parametros = parametros[::-1]
 logging.critical('Obtenida la lista de posibles parametros')
@@ -97,7 +89,6 @@ for param in itertools.product(*parametros):
     project_name = 'Model{num}'.format(num=cont)
     logging.critical('Preparado para lanzar stacks')
 
-    # sys.stdout.write("Preparado para lanzar stack\n")
     #Llamadas a rancher-compose
     call([
         './exec/rancher-compose',
